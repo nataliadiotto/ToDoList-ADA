c89c851b9483efbaefe03c8a15e011221b021cba
Natália Diotto <diottonatalia@gmail.com>
2024-02-16 21:06:26 -0300

    Creating queries

--- todolist/src/main/java/tech/ada/todolist/domain/ToDoController.java
+++ todolist/src/main/java/tech/ada/todolist/domain/ToDoController.java
@@ -64,7 +64,7 @@ public class ToDoController {
     }
 
 
-
+//implementar query manual
 
 
     @PatchMapping("/todo-item/{id}")
--- todolist/src/main/java/tech/ada/todolist/domain/ToDoItemRepository.java
+++ todolist/src/main/java/tech/ada/todolist/domain/ToDoItemRepository.java
@@ -1,6 +1,7 @@
 package tech.ada.todolist.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +11,18 @@ public interface ToDoItemRepository extends JpaRepository<ToDoItem, Long> {
 //<ToDoItem, Long> -> especifiquei a tabela (nome da classe) e o tipo da primary key (long)
 
    //o propio JPA ja reconhece que existe o atributo titulo e cria a query
-    public List<ToDoItem> findByTitle(String title);
+    List<ToDoItem> findByTitle(String title);
+
+
+    //criando uma query manual
+    @Query("SELECT t FROM ToDoItem t WHERE t.titulo = ?1")
+    List<ToDoItem> findByTitleQuery (String title);
+
+    //criando uma query nativa
+    @Query(value = "SELECT * FROM ToDoItem WHERE title = ?1", nativeQuery = true)
+    List<ToDoItem> findByTitleNativeQuery (String title);
+
+
 
 
 }
